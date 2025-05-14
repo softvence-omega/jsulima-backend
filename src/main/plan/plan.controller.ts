4f56eb46900f7c60e8bd4f7a45973e56777d6c04
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-05-14 15:59:23 +0600

    plan module attached

--- src/main/plan/plan.controller.ts
+++ src/main/plan/plan.controller.ts
@@ -1,34 +1,72 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+// import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+// import { PlanService } from './plan.service';
+// import { CreatePlanDto, UpdatePlanDto } from './create-plan.dto';
+
+
+// @Controller('subscriptions')
+// export class PlanController {
+//   constructor(private readonly service: PlanService) {}
+
+//   @Post()
+//   create(@Body() dto: CreatePlanDto) {
+//     return this.service.create(dto);
+//   }
+
+//   @Get()
+//   findAll() {
+//     return this.service.findAll();
+//   }
+
+//   @Get(':id')
+//   findOne(@Param('id') id: string) {
+//     return this.service.findOne(id);
+//   }
+
+//   @Patch(':id')
+//   update(@Param('id') id: string, @Body() dto: UpdatePlanDto) {
+//     return this.service.update(id, dto);
+//   }
+
+//   @Delete(':id')
+//   remove(@Param('id') id: string) {
+//     return this.service.remove(id);
+//   }
+// }
+
+
+
+
+import { Controller, Post, Body, Get, Param, Patch, Delete } from '@nestjs/common';
 import { PlanService } from './plan.service';
 import { CreatePlanDto, UpdatePlanDto } from './create-plan.dto';
 
 
-@Controller('subscriptions')
+@Controller('plans')
 export class PlanController {
-  constructor(private readonly service: PlanService) {}
+  constructor(private readonly planService: PlanService) {}
 
   @Post()
   create(@Body() dto: CreatePlanDto) {
-    return this.service.create(dto);
+    return this.planService.create(dto);
   }
 
   @Get()
   findAll() {
-    return this.service.findAll();
+    return this.planService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.service.findOne(id);
+    return this.planService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() dto: UpdatePlanDto) {
-    return this.service.update(id, dto);
+    return this.planService.update(id, dto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.service.remove(id);
+    return this.planService.remove(id);
   }
 }
