423c74e9c76b0f9cb05bbe7826b6950e4353fdfd
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-05-18 16:31:46 +0600

    cloudinary storage configuration updated

--- src/config/cloudinary.storage.ts
+++ src/config/cloudinary.storage.ts
@@ -9,13 +9,14 @@ cloudinary.config({
   });
 
   
-export const cloudinaryStorage = new CloudinaryStorage({
-  cloudinary,
-  params: async (req, file) => {
-    return {
-      folder: 'user_profiles',
-      format: file.mimetype.split('/')[1], // automatically extract file format
-      transformation: [{ width: 300, height: 300, crop: 'limit' }],
-    };
-  },
-});
+  export const cloudinaryStorage = new CloudinaryStorage({
+    cloudinary,
+    params: async (req, file) => {
+      return {
+        folder: 'user_profiles',
+        format: file.mimetype.split('/')[1], 
+        transformation: [{ width: 300, height: 300, crop: 'limit' }],
+        resource_type: 'auto', 
+      };
+    },
+  });
