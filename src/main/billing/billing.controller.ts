52a3afcfa9b4acf535abceed43f495542d3cf5e0
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-05-20 09:09:03 +0600

    minor issue fixed

--- src/main/billing/billing.controller.ts
+++ src/main/billing/billing.controller.ts
@@ -1,15 +1,25 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { StripeService } from '../stripe/stripe.service';
 import { CreateCheckoutDto } from './create-checkout.dto';
-
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('payment')
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth('access-token')
 export class BillingController {
   constructor(private stripeService: StripeService) {}
 
   @Post('checkout')
-  async createCheckout(@Body() body: CreateCheckoutDto) {
-    const { userId, planId, amount } = body;
+  async createCheckout(@Req() req, @Body() body: Omit<CreateCheckoutDto, 'userId'>) {
+    const userId = req.user.sub; // get authenticated user ID securely
+    const { planId, amount } = body;
 
     const successUrl = 'http://localhost:3000/success';
     const cancelUrl = 'http://localhost:3000/cancel';
